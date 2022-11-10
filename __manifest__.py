d24d9b76bef52e8b0e5706c68fd8e8121423de72
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-10 06:57:24 +0100

    Make the module more standard by using existing sale order lines default views.

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Sale order line menu & view',
-    'version': '1.0.1',
+    'version': '1.0.1.1',
     'author':'Soft-integration',
     'category': 'Sale',
     'description': "",
