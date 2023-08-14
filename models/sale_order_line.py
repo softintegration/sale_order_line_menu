30b138c41e5cddd947df8466d1104e52a887b4e1
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-08-14 15:49:22 +0100

    Sale order line:Add the group by «Product type»

---
+++ models/sale_order_line.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields,api,_
+from odoo.exceptions import UserError
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    type = fields.Selection(related='product_id.type',store=True)
+
